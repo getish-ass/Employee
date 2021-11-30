eb2ce5846255acf0ed1e2a884500cbaa4e4e9356
Getahun <getitpro@gmail.com>
2021-11-30 16:12:39 +0100

    Add project files.

---
+++ Employee/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Employee
+{
+    class Program
+    {
+        public string employeeName;
+        public int employeeSalary;
+
+
+        public void EmployeeDetails(string name, int salary)
+        {
+
+            employeeName = name;
+            employeeSalary = salary;
+        }
+        public void DesplayDetails()
+        {
+            Console.WriteLine("Employee Name: " + employeeName);
+            
+            Console.WriteLine("Salary: " + employeeSalary);
+           
+        }
+
+        static void Main(string[] args)
+        {
+            Program employee1 = new Program();
+
+            Console.WriteLine("Enter Employee Name");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter Salary");
+           int salary = Console.Read();
+
+            employee1.EmployeeDetails(name, salary);
+            employee1.DesplayDetails();
+            
+        }
+    }
+}
